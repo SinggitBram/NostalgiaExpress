8b015f3bf3f417b6b3bc6147acd45e9679fd6a2e
SinggitBram <bambadom@gmail.com>
2021-01-25 04:02:32 +0700

    re-learning express

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const logger = require("./middleware/logger");
+const app = express();
+
+//init middleware
+app.use(logger);
+//set static folder
+app.use(express.static(path.join(__dirname, "public")));
+//members api route
+app.use("/api/members", require("./routes/api/members"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
